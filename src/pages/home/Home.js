73d770090028141ddd7ae0c3ecb01e3a3413d9c1
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2023-04-12 02:34:59 +0600

    job category added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
+import JobCategoryList from '../../components/jobCategoryList/JobCategoryList'
 
 const Home = () => {
     return (
         <div>
             <Hero/>
+            <JobCategoryList/>
         </div>
     )
 }
